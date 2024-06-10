59c62a51552097213180b19215d1e02725c9becd
Evgeny <117734025+JeffreyWillims@users.noreply.github.com>
2024-06-10 10:12:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+print(len("i love python"))               #Задача номер 1
+
+
+first=10                                  #Задача номер 2
+second=60
+summa=first+second
+diff=first-second
+print(summa)
+print(diff)
+
+
+first=7, 8, 9                             #Задача номер 3
+second=15, 18, 40
+third=49, 60, 90
+mean=first + second + third
+mean = sum(mean) / len(mean)
+print(mean)
+
+first_string="Вторник"                    #Задача номер 4
+second_string="Понедельник"
+print(second_string ,first_string)
+
+a=5                                       #Задача номер 5
+b=7
+c=33
+f=(a*b)+(a*c)
+print(f)
+f=200 ** 3 / 2
+print(f)
